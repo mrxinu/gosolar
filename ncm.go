775203659c909d0ff3fd960e01eacd6f52754838
Kris Nova <kris@nivenly.com>
2017-06-11 10:21:01 -0600

    Unit Tests
     - Adding unit tests, and make target to bootstrap tests
     - Refactor remove function for testability
     - Working tests (assumes go is installed)

--- ncm.go
+++ ncm.go
@@ -2,14 +2,24 @@ package gosolar
 
 import "fmt"
 
-// RemoveNCMNodes is awesome.
+// RemoveNCMNodes is now even more awesome.
 func (c *Client) RemoveNCMNodes(guids []string) error {
-	req := [][]string{guids}
+	req, endpoint := getRemoveNCMNodesRequest(guids)
+	_, err := c.post(endpoint, req)
 
-	_, err := c.post("Invoke/Cirrus.Nodes/RemoveNodes", req)
 	if err != nil {
 		return fmt.Errorf("failed to remove the NCM nodes %v", err)
 	}
 
 	return nil
+}
+
+// RemoveNodeEndpoint is the endpoint to send the post request to remove NCM Nodes
+const RemoveNodeEndpoint_v1 = "Invoke/Cirrus.Nodes/RemoveNodes"
+
+// getRemoveNCMNodesRequest is a function that will convert a slice of guid strings into
+// an endpoint and a request that the API expects.
+func getRemoveNCMNodesRequest(guids []string) ([][]string, string) {
+	req := [][]string{guids}
+	return req, RemoveNodeEndpoint_v1
 }
