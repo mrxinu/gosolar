95e5097483c747ae9126d7533fa250ade1310a65
Steven Klassen <sklassen@gmail.com>
2017-07-31 02:58:32 -0700

    Remove const and function creating request

--- ncm.go
+++ ncm.go
@@ -4,7 +4,9 @@ import "fmt"
 
 // RemoveNCMNodes is now even more awesome.
 func (c *Client) RemoveNCMNodes(guids []string) error {
-	req, endpoint := getRemoveNCMNodesRequest(guids)
+	endpoint := "Invoke/Cirrus.Nodes/RemoveNodes"
+	req := [][]string{guids}
+
 	_, err := c.post(endpoint, req)
 
 	if err != nil {
@@ -12,14 +14,4 @@ func (c *Client) RemoveNCMNodes(guids []string) error {
 	}
 
 	return nil
-}
-
-// RemoveNodeEndpoint is the endpoint to send the post request to remove NCM Nodes
-const RemoveNodeEndpoint = "Invoke/Cirrus.Nodes/RemoveNodes"
-
-// getRemoveNCMNodesRequest is a function that will convert a slice of guid strings into
-// an endpoint and a request that the API expects.
-func getRemoveNCMNodesRequest(guids []string) ([][]string, string) {
-	req := [][]string{guids}
-	return req, RemoveNodeEndpoint
 }
