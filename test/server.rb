e3726e0c76e7d2f86245fa60074a78d708019c24
stobias123 <stobias123@gmail.com>
2019-12-05 18:51:56 -0800

    adding test server

--- test/server.rb
+++ test/server.rb
@@ -21,11 +21,56 @@ end
     } 
 =end
     
+subnet_obj = '''
+{
+    "totalRows":1,
+        "results": [
+        {
+            "SubnetId": 1234,
+            "Address": "10.199.152.0",
+            "CIDR": "23",
+            "FriendlyName": "test subnet",
+            "DisplayName": "test subnet",
+            "AvailableCount": 200,
+            "ReservedCount": 2,
+            "UsedCount": 181,
+            "totalCount": 512,
+            "Comments": "NFS - VLAN 410",
+            "VLAN": 410,
+            "AddressMask": "255.255.254.0"
+        }
+    ]
+}
+'''
+
+post '/SolarWinds/InformationService/v3/Json/Invoke/IPAM.SubnetManagement/GetFirstAvailableIp' do
+  return '"192.168.1.23"'
+end
 
 post '/SolarWinds/InformationService/v3/Json/Query' do
     data = JSON.parse( request.body.read.to_s )
     ## Get the post params
     puts data["parameters"]
+    ipam_query = data["query"].include? "IPAM"
+    if ipam_query
+        return '{"totalRows":1,
+        "results": [
+            {
+                "SubnetId": 1234,
+                "Address": "10.199.152.0",
+                "CIDR": 23,
+                "FriendlyName": "test subnet",
+                "DisplayName": "test subnet",
+                "AvailableCount": 200,
+                "ReservedCount": 2,
+                "UsedCount": 181,
+                "totalCount": 512,
+                "Comments": "NFS - VLAN 410",
+                "VLAN": "410",
+                "AddressMask": "255.255.254.0"
+            }
+        ]}'
+    end
     if data['parameters']['name'].include? "test"
         return '{"totalRows":1,
         "results": [
@@ -66,4 +111,8 @@ post '/SolarWinds/InformationService/v3/Json/Query' do
         ]}'
     end
     return '{"results":[]}'
+end
+
+post '/SolarWinds/InformationService/v3/Json/Invoke/IPAM.SubnetManagement/ChangeIPStatus' do
+    return '[{"IPNodeID": 123,"Address": "192.168.33.2","Status": 2,"StatusString": "Available","Comments": ""}]'
 end
